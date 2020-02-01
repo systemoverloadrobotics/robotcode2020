55ae5cfab12536555558dcd75e8e9048459ec149
yoshiyahoo <josiahtroup@gmail.com>
2020-01-31 19:35:47 -0800

    Create Elevator.java

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+
+    public Elevator() {
+
+    }
+
+    public void goUp() {
+
+    }
+
+    public void goDown() {
+
+    }
+
+    public void spinWinchForward() {
+
+    }
+
+    public void spinWinchBackward() {
+
+    }
+
+    public void stall() {
+
+    }
+}
