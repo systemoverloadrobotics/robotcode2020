18640528ed8df1ee6efdfdaf8888cb3184373597
yoshiyahoo <josiahtroup@gmail.com>
2020-01-31 19:41:23 -0800

    Update Climb.java

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Elevator extends SubsystemBase {
+public class Climb extends SubsystemBase {
 
-    public Elevator() {
+    public Climb() {
 
     }
 
