fca596c500cff6d1e2583f969af2a26b811620c5
anuashi <anuashigaur@gmail.com>
2020-01-31 19:38:48 -0800

    new outtake class

---
+++ src/main/java/frc/robot/commands/Outtake.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Outtake extends SubsystemBase {
+    /**
+     * Creates a new ExampleSubsystem.
+     */
+    public Outtake() {
+
+    }
+    public static void spin(){
+
+    }
+    public static void stop(){
+
+
+    }
+
+
+}
