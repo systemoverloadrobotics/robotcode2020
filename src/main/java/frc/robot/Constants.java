983e299223fed2a21ce6d5c5016f80084c47c02e
yoshiyahoo <josiahtroup@gmail.com>
2020-02-01 14:32:09 -0800

    Joystick

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants.  This class should not be used for any other purpose.  All constants should be
