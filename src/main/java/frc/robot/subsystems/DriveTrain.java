457c474db57cb65ba51569d1b3abdc82241bb7b7
yoshiyahoo <josiahtroup@gmail.com>
2020-01-31 19:30:04 -0800

    Create DriveTrain.java

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+
+    public DriveTrain() {
+
+    }
+
+    public void moveForward() {
+
+    }
+
+    public void moveBackward() {
+
+    }
+
+    public void turnLeft() {
+
+    }
+
+    public void turnRight() {
+
+    }
+
+    public void park() {
+
+    }
+}
