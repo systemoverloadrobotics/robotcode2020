0131bb008db9ce7a5f61379b294b9114761d447b
yoshiyahoo <josiahtroup@gmail.com>
2020-02-01 19:24:03 -0800

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.Joystick;
