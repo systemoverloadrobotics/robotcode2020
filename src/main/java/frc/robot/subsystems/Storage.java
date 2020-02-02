9c9b554054a880e009f816325e40be34208f286d
Tori Moore <52933665+VMoore20@users.noreply.github.com>
2020-02-01 19:52:24 -0800

    Made Storage File and Functions

---
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+
+    public Storage() {
+
+    }
+
+    public void runPolycordForward(){
+
+    }
+
+    public void runPolycordBackward(){
+
+    }
+
+    public boolean[] ballPositions(){
+
+    }
+    
+}
