5c3c229c1aa565268fa4931920e238c9b03928eb
roslyn-j-l <56180466+roslyn-j-l@users.noreply.github.com>
2020-02-01 19:07:38 -0800

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -3,15 +3,15 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
 
 public class DriveTrain extends SubsystemBase {
-    private CANSparkMax rightMotorCont = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax rightMotor1 = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax rightMotor2 = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax leftMotorCont = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax leftMotor1 = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax leftMotor2 = new CANSparkMax(1, MotorType.kBrushless);
+    private CANSparkMax RIGHT_MASTER_ID = new CANSparkMax(DriveConstants.RIGHT_MASTER_ID, MotorType.kBrushless);
+    private CANSparkMax RIGHT_FOLLOWER_1_ID = new CANSparkMax(DriveConstants.RIGHT_FOLLOWER_1_ID, MotorType.kBrushless);
+    private CANSparkMax RIGHT_FOLLOWER_2_ID = new CANSparkMax(DriveConstants.RIGHT_FOLLOWER_2_ID, MotorType.kBrushless);
+    private CANSparkMax LEFT_MASTER_ID = new CANSparkMax(DriveConstants.LEFT_MASTER_ID, MotorType.kBrushless);
+    private CANSparkMax LEFT_FOLLOWER_1_ID = new CANSparkMax(DriveConstants.LEFT_FOLLOWER_1_ID, MotorType.kBrushless);
+    private CANSparkMax LEFT_FOLLOWER_2_ID = new CANSparkMax(DriveConstants.LEFT_FOLLOWER_2_ID, MotorType.kBrushless);
 
 
     public DriveTrain() {
