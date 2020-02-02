a938e60c314da7121168095aefb6541afd690353
Tori Moore <52933665+VMoore20@users.noreply.github.com>
2020-02-01 19:22:35 -0800

    Implemented Functions

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,26 +1,26 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 
 public class Intake extends SubsystemBase {
 
+    public static final WPI_VictorSPX intakeFlywheels = new WPI_VictorSPX(Constants.INTAKE_WHEELS_ID);
+
     public Intake(){
-
-
     }
 
     public void spinIn(){
-
-
+        intakeFlywheels.set(ControlMode.PercentOutput, .2);
     }
 
     public void spinOut(){
-
-
+        intakeFlywheels.set(ControlMode.PercentOutput, -.2);
     }
 
     public void stop(){
-        
+        intakeFlywheels.set(ControlMode.PercentOutput, 0);
     }
 
 }
