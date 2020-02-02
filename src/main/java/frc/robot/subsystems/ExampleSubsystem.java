86e2c56f970c2d95d7cd9c39e8ab7989f97bb8af
palak guleria <44914202+Palak-Guleria@users.noreply.github.com>
2020-02-01 18:59:10 -0800

    Update ExampleSubsystem.java

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -9,7 +9,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemSubsystemBaseBase {
+public class ExampleSubsystem extends SubsystemBase {
   /**
    * Creates a new ExampleSubsystem.
    */
