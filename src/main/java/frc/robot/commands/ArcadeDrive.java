648869c7e0fe3db6a4efee9cd5cf27dc11eb9327
yoshiyahoo <josiahtroup@gmail.com>
2020-02-02 19:51:54 -0800

    removed initialize command from ArcadeDrive

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -16,12 +16,6 @@ public class ArcadeDrive extends CommandBase {
         addRequirements(arcadeDrive);
     }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() { //once
-
-    }
-
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
