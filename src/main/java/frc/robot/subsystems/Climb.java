a132677e779012db88bd01d4dc39299482ad6866
yoshiyahoo <josiahtroup@gmail.com>
2020-02-02 22:00:24 -0800

    changed import from "frc.robot.Constants" to "frc.robot.Constants.ClimbConstants"

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -4,18 +4,18 @@ import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.RemoteLimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.ClimbConstants;
 
 public class Climb extends SubsystemBase {
 
-    public WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(Constants.ClimbConstants.CLIMB_ELEVATOR_ID);
-    public WPI_TalonSRX winchMotor = new WPI_TalonSRX(Constants.ClimbConstants.CLIMB_WINCH_ID);
+    public WPI_TalonSRX elevatorMotor = new WPI_TalonSRX(ClimbConstants.CLIMB_ELEVATOR_ID);
+    public WPI_TalonSRX winchMotor = new WPI_TalonSRX(ClimbConstants.CLIMB_WINCH_ID);
 
     public Climb() {
-        elevatorMotor.configForwardLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyOpen,Constants.ClimbConstants.CLIMB_ELEVATOR_ID,0);
-        elevatorMotor.configReverseLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyClosed,Constants.ClimbConstants.CLIMB_ELEVATOR_ID,0);
+        elevatorMotor.configForwardLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyOpen,ClimbConstants.CLIMB_ELEVATOR_ID,0);
+        elevatorMotor.configReverseLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyClosed,ClimbConstants.CLIMB_ELEVATOR_ID,0);
 
-        winchMotor.configForwardLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyOpen,Constants.ClimbConstants.CLIMB_WINCH_ID,0);
+        winchMotor.configForwardLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyOpen,ClimbConstants.CLIMB_WINCH_ID,0);
 
         elevatorMotor.configFactoryDefault();
         winchMotor.configFactoryDefault();
