147e72616363470496f76bd6c32cf651c74c626d
Tori Moore <52933665+VMoore20@users.noreply.github.com>
2020-02-05 19:12:45 -0800

    Made Basic Polycord Functions

--- src/main/java/frc/robot/subsystems/Storage.java
+++ src/main/java/frc/robot/subsystems/Storage.java
@@ -1,24 +1,36 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Storage extends SubsystemBase {
+
+    public static final DigitalInput ballsInSwitch = new DigitalInput(StorageConstants.BALLS_IN_SWITCH_ID);
+    public static final WPI_VictorSPX polycord = new WPI_VictorSPX(StorageCosntants.POLYCORD_MOTOR_ID);
 
     public Storage() {
 
     }
 
     public void runPolycordForward(){
-
+        intakeFlywheels.set(ControlMode.PercentOutput, .2);
     }
 
     public void runPolycordBackward(){
-
+        intakeFlywheels.set(ControlMode.PercentOutput, -.2);
     }
 
-    public boolean[] ballPositions(){
+    public void stopPolycord(){
+        intakeFlywheels.set(ControlMode.PercentOutput, 0);
+    }
 
-        return new boolean[1]; //placeholder for the return type
+    private boolean loadingIncomingBalls(){
+        if (!ballsInSwitch.get()) {
+            runPolycordForward();
+            return true;
+        }
+        stopPolycord();
+        return false;
     }
     
 }
