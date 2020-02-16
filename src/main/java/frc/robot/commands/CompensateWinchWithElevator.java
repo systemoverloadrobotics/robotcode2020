5e9edac40478d8dedc7066601504940ea5785eca
yoshiyahoo <josiahtroup@gmail.com>
2020-02-15 19:55:54 -0800

    made commands

---
+++ src/main/java/frc/robot/commands/CompensateWinchWithElevator.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class CompensateWinchWithElevator extends CommandBase {
+
+    public CompensateWinchWithElevator(){
+
+    }
+    @Override
+    public void initialize(){
+
+    }
+    @Override
+    public void execute(){
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
+
