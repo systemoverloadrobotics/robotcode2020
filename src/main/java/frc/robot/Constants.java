c045eb73775b5793d21fb24a238979671c259586
yoshiyahoo <josiahtroup@gmail.com>
2020-02-20 18:38:41 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,8 +52,6 @@ public final class Constants {
         public static final int DOUBLESOLENOID_REVERSECHANNEL_ID = 1;
         public static final int BALLS_IN_SWITCH_ID = 17;
         public static final int BALLS_OUT_SWITCH_ID = 18;
-        public static final double POLYCORD_FORWARD_SPEED = .2;
-        public static final double POLYCORD_BACKWARD_SPEED = .2;
     }
     public static final class OuttakeConstants {
         public static final int OUTTAKE_WHEELS_ID = 14;
