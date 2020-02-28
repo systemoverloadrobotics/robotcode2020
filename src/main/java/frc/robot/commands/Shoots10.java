e1b6b7f1cc7973a710317158751d24ff59725a4f
Michael Crandall <michaelcrandall33@gmail.com>
2020-02-27 20:22:38 -0800

    fixed naming

--- src/main/java/frc/robot/commands/Shoots10.java
+++ src/main/java/frc/robot/commands/Shoots10.java
@@ -35,7 +35,7 @@ public class Shoots10 extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_shoot.stopShoot();
+    m_shoot.spinStop();
   }
 
   // Returns true when the command should end.
