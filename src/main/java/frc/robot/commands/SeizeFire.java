cbe1d1474f956003351513641940aa24feb17027
Basil Khwaja <khwajabasil@gmail.com>
2020-02-27 19:43:10 -0800

    Override

--- src/main/java/frc/robot/commands/SeizeFire.java
+++ src/main/java/frc/robot/commands/SeizeFire.java
@@ -3,18 +3,17 @@ package frc.robot.commands;
 import frc.robot.Constants.OuttakeConstants;
 import frc.robot.subsystems.Outtake;
 
-private Outtake m_outtake;
+private final Outtake m_outtake;
 
 public class SeizeFire extends CommandBase {
 
     public void initialize{
-
         m_outtake.spinStop();
-
     }
+
+    @Override
     public void isFinished(){
-
         return true;
-
     }
+    @Override
 }
