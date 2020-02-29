3af0e8700d6cfcb5be98d02ff5ef564f9914c221
Tori Moore <52933665+VMoore20@users.noreply.github.com>
2020-02-28 19:23:33 -0800

    Intake Ball Command

---
+++ src/main/java/frc/robot/commands/IntakeBall.java
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Storage;
+
+
+public class IntakeBall extends CommandBase {
+
+  private final Intake in;
+  private final Storage store;
+
+  public IntakeBall(Intake intakeWheels, Storage ballStorage) {
+    in = intakeWheels;
+    store = ballStorage;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intakeWheels);
+  }
+
+  // Called when the command is initially scheduled.
+  public void initialize() { //once
+    in.spinIn();
+    in.extend();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  public void end(boolean interrupted) {
+    in.retract();
+    in.stop();
+  }
+
+  // Returns true when the command should end.
+  public boolean isFinished() {
+    return store.getPos0();
+  }
+}
