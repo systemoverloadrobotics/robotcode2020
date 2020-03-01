3434e94f5d7d98d443f3f79e0cfe77dad37b9edc
Michael Crandall <michaelcrandall33@gmail.com>
2020-02-29 17:11:35 -0800

    refactored merged files

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -7,27 +7,29 @@ import frc.robot.subsystems.DriveTrain;
 import java.util.function.DoubleSupplier;
 
 public class TankDrive extends CommandBase {
-    DriveTrain m_driveTrain;
-    DoubleSupplier m_leftSpeed;
-    DoubleSupplier m_rightSpeed;
-    public TankDrive(DriveTrain tankDrive, DoubleSupplier leftSpeed, DoubleSupplier rightSpeed){
-        m_driveTrain = tankDrive;
-        m_leftSpeed = leftSpeed;
-        m_rightSpeed = rightSpeed;
-    }
+	DriveTrain m_driveTrain;
+	DoubleSupplier m_leftSpeed;
+	DoubleSupplier m_rightSpeed;
 
-    @Override
-    public void initialize(){}
+	public TankDrive(DriveTrain driveTrain, DoubleSupplier leftSpeed, DoubleSupplier rightSpeed) {
+		m_driveTrain = driveTrain;
+		m_leftSpeed = leftSpeed;
+		m_rightSpeed = rightSpeed;
+		addRequirements(driveTrain);
+	}
 
-    @Override
-    public void execute(){
-        m_driveTrain.driveTank(m_leftSpeed.getAsDouble(), m_rightSpeed.getAsDouble());
-    }
+	@Override
+	public void execute() {
+		m_driveTrain.driveTank(m_leftSpeed.getAsDouble(), m_rightSpeed.getAsDouble());
+	}
 
-    @Override
-    public void end(boolean interrupted){m_driveTrain.driveTank(0,0);}
+	@Override
+	public void end(boolean interrupted) {
+		m_driveTrain.driveTank(0, 0);
+	}
 
-    @Override
-    public boolean isFinished(){return false;}
-
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
