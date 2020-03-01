88a5d1d9b663573918aef320d2d495617c57b5f3
Tori Moore <52933665+VMoore20@users.noreply.github.com>
2020-02-29 19:15:35 -0800

    ShootTheBall and needed ReadyToFire commands made

---
+++ src/main/java/frc/robot/commands/Outtake/ShootTheBall.java
@@ -0,0 +1,13 @@
+package frc.robot.commands.Outtake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class ShootTheBall extends SequentialCommandGroup {
+
+    public ShootTheBall (Command readyToFire, Command moveToShooter, Command ceaseFire, double rpm){
+        addCommands(readyToFire, moveToShooter, ceaseFire);
+    }
+
+}
+
---
+++ src/main/java/frc/robot/commands/ReadyToFire.java
@@ -0,0 +1,12 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+public class ReadyToFire extends ParallelCommandGroup {
+
+    public ReadyToFire (Command align, Command moveToChamber, Command warmUpMotor){
+        addCommands(align, moveToChamber, warmUpMotor);
+    }
+
+}
